eebff08e899457644f5a8d84c00102390e609968
swimly <979741120@qq.com>
2017-06-22 18:12:45 +0800

    添加消息列表

--- src/views/Message.vue
+++ src/views/Message.vue
@@ -2,15 +2,15 @@
   <div class="page gray">
     <group gutter="0px" v-if="list.length > 0">
       <cell value-align="left" v-for="(item, index) in list" :key="index">
-        <h2 class="time">{{item.time}}</h2>
-        <p class="text">{{item.title}}<badge v-if="!item.read"></badge></p>
+        <h2 class="time">{{item.createTime}}<badge v-if="!item.status"></badge></h2>
+        <p class="text">{{item.title}}</p>
       </cell>
     </group>
     <h2 class="tip" v-if="list.length === 0">您暂时没有任何消息！</h2>
   </div>
 </template>
 <script>
-  import {Group, Cell, Badge} from 'vux'
+  import {Group, Cell, Badge, dateFormat} from 'vux'
   import {message} from '../config'
   export default {
     components: {
@@ -30,13 +30,17 @@
         jsonp: 'callback',
         jsonpCallback: 'json',
         params: {
-          userId: JSON.parse(this.$localStorage.get('userInfo')).userId,
+          // userId: JSON.parse(this.$localStorage.get('userInfo')).userId,
+          userId: '0e9236d6b4474f259cfd1d2f9bf8f3b0',
           limit: 10,
           pageIndex: 0
         }
       })
       .then(res => {
         console.log(res)
+        res.body.data.messageList.forEach(el => {
+          el.createTime = dateFormat(el.createTime)
+        })
         if (res.body.status) {
           this.list = res.body.data.messageList
         } else {
@@ -54,6 +58,7 @@
 </script>
 <style scoped>
 .time{font-size:1rem;}
-.text{font-size:1.1rem;color:#5B5B5B;line-height:2;}
+.text{font-size:1.1rem;color:#5B5B5B;line-height:1.6;word-break: break-all;display:-webkit-box;-webkit-box-orient: vertical;-webkit-line-clamp:2;overflow:hidden;}
 .tip{font-size:1.2rem;color:#999;text-align:center;padding:3rem 0;}
+.time .vux-badge{position:absolute;right:1rem;top:1rem;}
 </style>
