fcc459634c682130b91695266d6168e69fff2a9d
swimly <979741120@qq.com>
2017-06-28 09:43:38 +0800

    优化大小

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import FastClick from 'fastclick'
 import App from './App'
 import router from './router'
 import store from './store'
-import {WechatPlugin, LoadingPlugin, ToastPlugin} from 'vux'
+import {LoadingPlugin, ToastPlugin} from 'vux'
 import VueLazyload from 'vue-lazyload'
 import VueHead from 'vue-head'
 import VueResource from 'vue-resource'
@@ -13,12 +13,12 @@ import VueLocalStorage from 'vue-localstorage'
 Vue.use(VueLocalStorage)
 Vue.use(LoadingPlugin)
 FastClick.attach(document.body)
-import { InfiniteScroll } from 'mint-ui'
-Vue.use(InfiniteScroll)
-import VueScroller from 'vue-scroller'
-Vue.use(VueScroller)
+// import { InfiniteScroll } from 'mint-ui'
+// Vue.use(InfiniteScroll)
+// import VueScroller from 'vue-scroller'
+// Vue.use(VueScroller)
 Vue.config.productionTip = false
-Vue.use(WechatPlugin)
+// Vue.use(WechatPlugin)
 Vue.use(VueResource)
 Vue.use(VueHead)
 Vue.use(ToastPlugin)
